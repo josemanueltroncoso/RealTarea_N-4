6c6cc1538a2fcff0d14f372d6bcbe3745fa3fa65
Matias Contreras <matias.contrerasf@utem.cl>
2019-01-12 03:15:58 -0300

    fin

--- Funciones.R
+++ Funciones.R
@@ -1,3 +1,5 @@
+#5.-
+#función para generar RUT válidos
 dv <- function(rut){
   rut = as.character(rut)
   x = as.numeric(rev(strsplit(rut,NULL)[[1]]))
@@ -10,3 +12,39 @@ dv <- function(rut){
   return(dv)
 }
 
+#test
+dv(19296595)
+
+#lista para ejecutar 5.000 Ruts válidos
+Rut_creados<-list()
+  t<-proc.time ()
+    for ( i  in sample ( 0000000 : 9999999 ,5000 ,replace = F )) {
+      rut <- print (paste ( i , " - " , dv ( i )))
+        Ruts_validos <- c ( Rut_creados , rut )
+
+}
+#tiempo en recorre la lista de RUT realizados
+proc.time () -  t
+#test
+#user   system   elapsed 
+1.270   0.120   4.104 
+
+
+#6.-
+
+lista<-" porque la llama que llama estando en llamas me llama, alguien más llama "
+splitLista<-strsplit(lista, " ")[[1]]
+splitEspacioLista<-tolower(splitLista)
+unlistLista<-unlist(splitEspacioNoticia)
+tablapalabras<-table(unlistLista)
+dfPalabrasNoticia<-as.data.frame(tablapalabras)
+
+#8.-
+list()
+contarSaldoNegativo<-function(saldoCuenta){
+  if(saldoCuenta<0){
+    +1
+  }else{
+    0 
+  }
+}
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # RealTarea_N-4
+
+#dv: double->string
+#DEF dv: si se introduce el RUT, entregar el dígito verificador.
+#ejemplo dv(19296595)->0
+
+#t: Double->string
+#def: t: crea rut con dígitos verificador y las adhiere a una lista.
+
+#contarSaldoNegativo
